264f4b6fa7f97183a6db6f4ebe390d4d288fbd59
lvsousha <zclvideo@163.com>
2017-05-04 14:40:08 +0800

    重构新建节点和关系的方法

--- graph-database.neo4j/src/main/java/com/lvdousha/graph_database/neo4j/EmbeddedNeo4j.java
+++ graph-database.neo4j/src/main/java/com/lvdousha/graph_database/neo4j/EmbeddedNeo4j.java
@@ -2,9 +2,12 @@ package com.lvdousha.graph_database.neo4j;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.Relationship;
@@ -12,24 +15,33 @@ import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.graphdb.schema.Schema;
 import org.neo4j.graphdb.traversal.Evaluation;
 import org.neo4j.graphdb.traversal.Evaluator;
+import org.neo4j.graphdb.traversal.Paths;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.graphdb.traversal.Uniqueness;
 import org.neo4j.io.fs.FileUtils;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class EmbeddedNeo4j {
 	// Embedded Neo4j会在本地产生一个文件夹(类似于Mysql的数据库)
-	private static final String DB_PATH = "neo4j/db";
+	private static final String DB_PATH = "target/neo4j/db/";
 	private static EmbeddedNeo4j instance;
-
-	public String greeting;
-
-	// START SNIPPET: vars
 	private static GraphDatabaseService graphDb;
+	public static Logger log = LoggerFactory.getLogger(EmbeddedNeo4j.class);
 	
 	
+	public static void main(final String[] args) throws IOException {
+//		FileUtils.deleteRecursively(new File(DB_PATH));
+//		EmbeddedNeo4j embeddedNeo4j = EmbeddedNeo4j.getInstance();
+//		embeddedNeo4j.search();
+//		embeddedNeo4j.removeData();
+//		embeddedNeo4j.shutDown();
+	}
 	
 	private EmbeddedNeo4j() {
 		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(DB_PATH));
@@ -46,50 +58,51 @@ public class EmbeddedNeo4j {
 		}
 		return instance;
 	}
-
-	public static void main(final String[] args) throws IOException {
-		FileUtils.deleteRecursively(new File(DB_PATH));
-		EmbeddedNeo4j embeddedNeo4j = EmbeddedNeo4j.getInstance();
-		embeddedNeo4j.createNode();
-		embeddedNeo4j.search();
-		// 删除数据
-//		embeddedNeo4j.removeData();
-		embeddedNeo4j.shutDown();
+	
+	public void createNode(Map<String,Object> model, String label) {
+		try (Transaction tx = graphDb.beginTx()) {
+			Index<Node> nodeIndex = graphDb.index().forNodes(label);
+			Node node = graphDb.createNode();
+			node.addLabel(Label.label(label));
+			for(String key : model.keySet()){
+				node.setProperty(key, model.get(key));
+			}
+			nodeIndex.add(node, label , model.get(label));
+			tx.success();
+		}
 	}
 	
-	public void createNode() {
+	public void createIndex(String label, String key){
+		IndexDefinition indexDefinition;
+		try (Transaction tx = graphDb.beginTx()){
+		    Schema schema = graphDb.schema();
+		    indexDefinition = schema.indexFor( Label.label(label) )
+		            .on(key)
+		            .create();
+		    tx.success();
+		}
+		try (Transaction tx = graphDb.beginTx()){
+		    Schema schema = graphDb.schema();
+		    schema.awaitIndexOnline( indexDefinition, 10, TimeUnit.SECONDS );
+		}
+	}
+	
+	public void createNodeAccordingToIndex(Map<String,Object> model, String label){
+		try (Transaction tx = graphDb.beginTx()){
+	        Node node = graphDb.createNode(Label.label(label));
+	        for(String key : model.keySet()){
+				node.setProperty(key, model.get(key));
+			}
+		    tx.success();
+		}
+	}
+	
+	public void createRelationship(Node start, Node end, RelTypes reltype, Map<String,Object> properties){
 		try (Transaction tx = graphDb.beginTx()) {
-			Index<Node> nodeIndex = graphDb.index().forNodes( "nodes" );
-			Node firstNode;
-			Node secondNode;
-			Node thirdNode;
-			Node fourthNode;
-			Node fifthNode;
-			firstNode = graphDb.createNode();
-			firstNode.setProperty("name", "Joe");
-			secondNode = graphDb.createNode();
-			secondNode.setProperty("name", "Tom");
-			thirdNode = graphDb.createNode();
-			thirdNode.setProperty("name", "Jerry");
-			fourthNode = graphDb.createNode();
-			fourthNode.setProperty("name", "Lisa");
-			fifthNode = graphDb.createNode();
-			fifthNode.setProperty("name", "Kaly");
-			
-			nodeIndex.add( firstNode, "name", "Joe" );
-			nodeIndex.add( secondNode, "name", "Tom" );
-			nodeIndex.add( thirdNode, "name", "Jerry" );
-			nodeIndex.add( fourthNode, "name", "Lisa" );
-			nodeIndex.add( fifthNode, "name", "Kaly" );
-
-			Relationship relationship1 = firstNode.createRelationshipTo(secondNode, RelationshipType.withName("TRANSFER"));
-			relationship1.setProperty("amount", 100);
-			Relationship relationship2 = secondNode.createRelationshipTo(thirdNode, RelTypes.TRANSFER);
-			relationship2.setProperty("amount", 200);
-			Relationship relationship3 = thirdNode.createRelationshipTo(fourthNode, RelTypes.TRANSFER);
-			relationship3.setProperty("amount", 300);
-			Relationship relationship4 = firstNode.createRelationshipTo(fifthNode, RelTypes.TRANSFER);
-			relationship4.setProperty("amount", 400);
+			Relationship relationship = start.createRelationshipTo(end, reltype);
+			for(String key : properties.keySet()){
+				relationship.setProperty(key, properties.get(key));
+			}
 			tx.success();
 		}
 	}
@@ -100,7 +113,7 @@ public class EmbeddedNeo4j {
 			
 			Node user = nodeIndex.get( "name", "Joe" ).getSingle();
 			if(user != null){
-				System.out.println(user.getProperty("name"));
+				log.info(user.getProperty("name").toString());
 			}
 			TraversalDescription td = graphDb.traversalDescription()
 		            .breadthFirst()
@@ -120,8 +133,7 @@ public class EmbeddedNeo4j {
 			Traverser tranversal = td.traverse(user);
 			PathPrinter pathPrinter = new PathPrinter( "name" );
 			for (Path path : tranversal){
-//				System.out.println(path.toString());
-				System.out.println(org.neo4j.graphdb.traversal.Paths.pathToString(path, pathPrinter ));
+				log.info(Paths.pathToString(path, pathPrinter ));
 			}
 			tx.success();
 		}
