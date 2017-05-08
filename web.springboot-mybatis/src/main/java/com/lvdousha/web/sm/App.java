3593c24276649792d7dc76d99b231886de9dd4c2
lvsousha <zclvideo@163.com>
2017-05-08 10:22:16 +0800

    使用log4j记录日志

--- web.springboot-mybatis/src/main/java/com/lvdousha/web/sm/App.java
+++ web.springboot-mybatis/src/main/java/com/lvdousha/web/sm/App.java
@@ -2,6 +2,7 @@ package com.lvdousha.web.sm;
 
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,41 +11,34 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
-import com.alibaba.druid.pool.DruidDataSource;  
-  
+import com.alibaba.druid.pool.DruidDataSource;
 
 @SpringBootApplication
 @ServletComponentScan
 public class App {
-      public static void main(String[] args) {
-          SpringApplication.run(App.class, args);
-      }
-  	
-      protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-          // TODO Auto-generated method stub
-          return builder.sources(App.class);
-      }
-      
-      @Autowired
-      private Environment env;
-      
-      @Bean(destroyMethod = "close")
-      public DataSource dataSource() {
-    	  DruidDataSource datasource = new DruidDataSource();
-    	  datasource.setUrl(env.getProperty("spring.datasource.url"));
-    	  datasource.setUsername(env.getProperty("spring.datasource.username"));
-    	  datasource.setPassword(env.getProperty("spring.datasource.password"));
-    	  datasource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
-    	  return datasource;
-      }
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-} 
+	private static final Logger log = Logger.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(App.class);
+	}
+
+	@Autowired
+	private Environment env;
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		log.info("初始化DataSource");
+		DruidDataSource datasource = new DruidDataSource();
+		datasource.setUrl(env.getProperty("spring.datasource.url"));
+		datasource.setUsername(env.getProperty("spring.datasource.username"));
+		datasource.setPassword(env.getProperty("spring.datasource.password"));
+		datasource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+		return datasource;
+	}
+
+}
