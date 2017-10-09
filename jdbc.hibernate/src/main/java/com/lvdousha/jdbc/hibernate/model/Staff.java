4b5a352bdd26565c50f922209ec2744a50af5f72
zhengchanglin <yh.lvdousha@gmail.com>
2017-10-09 17:41:39 +0800

    jdbc.hibernate

---
+++ jdbc.hibernate/src/main/java/com/lvdousha/jdbc/hibernate/model/Staff.java
@@ -0,0 +1,41 @@
+package com.lvdousha.jdbc.hibernate.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Staff  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5091249010478377056L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+
+}
